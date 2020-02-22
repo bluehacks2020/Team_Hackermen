0411f8558c390465888326b1e40749f368881579
Terariver <hammerjack737@gmail.com>
2020-02-23 03:39:52 +0800

    test replaced with videopage

--- lib/test_screen.dart
+++ lib/test_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:blue_hacks_kuwento/splash_screen.dart';
 import 'dart:async';
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
 
 class TestScreen extends StatefulWidget {
   @override
@@ -8,18 +10,83 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen>{
-	@override
-  	Widget build(BuildContext context){
-    	return new Scaffold(
-      		body: Stack(
-        	fit: StackFit.expand,
-        	children:<Widget>[
-          		Container(
-            		decoration: BoxDecoration(color: Color.fromARGB(255,238,227,157),),
-          		),
-        	]
-    		)
-    	);
-	}
+  	@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alamat ng Pinya'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ChewieListItem(
+            videoPlayerController: VideoPlayerController.asset(
+              'assets/alam1_alamat.mp4',
+            ),
+            looping: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChewieListItem extends StatefulWidget {
+  // This will contain the URL/asset path which we want to play
+  final VideoPlayerController videoPlayerController;
+  final bool looping;
+
+  ChewieListItem({
+    @required this.videoPlayerController,
+    this.looping,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _ChewieListItemState createState() => _ChewieListItemState();
+}
+
+class _ChewieListItemState extends State<ChewieListItem> {
+  ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Wrapper on top of the videoPlayerController
+    _chewieController = ChewieController(
+      videoPlayerController: widget.videoPlayerController,
+      aspectRatio: 9 / 16,
+      // Prepare the video to be played and display the first frame
+      autoInitialize: true,
+      looping: widget.looping,
+      // Errors can occur for example when trying to play a video
+      // from a non-existent URL
+      errorBuilder: (context, errorMessage) {
+        return Center(
+          child: Text(
+            errorMessage,
+            style: TextStyle(color: Colors.white),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Chewie(
+        controller: _chewieController,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // IMPORTANT to dispose of all the used resources
+    widget.videoPlayerController.dispose();
+    _chewieController.dispose();
+  }
 }
 
