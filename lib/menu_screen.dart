40b50dc15f02da99489b3855dfdf4ebc3b8412d1
Terariver <hammerjack737@gmail.com>
2020-02-22 21:34:24 +0800

    Fixed Splash and Menu Photos

---
+++ lib/menu_screen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:blue_hacks_kuwento/splash_screen.dart';
+import 'dart:async';
+
+class MenuScreen extends StatefulWidget {
+  @override
+  _MenuScreenState createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255,209,238,157),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 15.0, left: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+                Container(
+                    width: 125.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.filter_list),
+                          color: Colors.white,
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.menu),
+                          color: Colors.white,
+                          onPressed: () {},
+                        )
+                      ],
+                    ))
+              ],
+            ),
+          ),
+          SizedBox(height: 25.0),
+          Padding(
+            padding: EdgeInsets.only(left: 40.0),
+            child: Row(
+              children: <Widget>[
+                Text('Kuwento',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0)),
+                SizedBox(width: 10.0),
+
+              ],
+            ),
+          ),
+          SizedBox(height: 40.0),
+          Container(
+            height: MediaQuery.of(context).size.height - 185.0,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
+            ),
+            child: ListView(
+              primary: false,
+              padding: EdgeInsets.only(left: 25.0, right: 20.0),
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(top: 45.0),
+                    child: Container(
+                        height: MediaQuery.of(context).size.height - 300.0,
+                        child: ListView(children: [
+                          _buildFoodItem('assets/home.png', 'Alamat ng Pinya', ''),
+                          _buildFoodItem('assets/home2.png', 'Noli me Tangre', ''),
+
+                        ]))),
+                    Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFoodItem(String imgPath, String foodName, String price) {
+    return Padding(
+        padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+        child: InkWell(
+          onTap: () {},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                child: Row(
+                  children: [
+                    Hero(
+                      tag: imgPath,
+                      child: Image(
+                        image: AssetImage(imgPath),
+                        fit: BoxFit.cover,
+                        height: 75.0,
+                        width: 75.0
+                      )
+                    ),
+                    SizedBox(width: 10.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children:[
+                        Text(
+                          foodName,
+                          style: TextStyle(
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold
+                          )
+                        ),
+                        Text(
+                          price,
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.grey
+                          )
+                        )
+                      ]
+                    )
+                  ]
+                )
+              ),
+              IconButton(
+                icon: Icon(Icons.add),
+                color: Colors.black,
+                onPressed: () {}
+              )
+            ],
+          )
+        ));
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:blue_hacks_kuwento/menu_screen.dart';
+import 'dart:async';
+
+
+class SplashScreen extends StatefulWidget{
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>{
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 3),()=> {Navigator.of(context).pushNamed('/MenuScreen')});
+
+  }
+
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children:<Widget>[
+          Container(
+            decoration: BoxDecoration(color: Color.fromARGB(255,238,227,157),),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 200.0,
+                        height: 100.0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255,238,227,157),
+                            image: DecorationImage(
+                              image: AssetImage('assets/splash_screen_title.png'), fit: BoxFit.cover,),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "kuwento",style: TextStyle(
+                          color: Colors.white,
+                          fontSize:24.0,
+                          fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(flex: 1,
+                child: Column(
+                  mainAxisAlignment:MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Padding(
+                      padding: EdgeInsets.only(top:20.0),
+                    ),
+                    Text("Kuwento Loading",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
