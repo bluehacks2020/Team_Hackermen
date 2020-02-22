c276ed209d344052baeba6e96cd95ca460f25999
Terariver <hammerjack737@gmail.com>
2020-02-22 14:44:01 +0800

    SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,63 @@ void main(){
 
 class MyApp extends StatelessWidget {
   build(BuildContext context){
-    return MaterialApp(home: Text('                                       KUWENTO'),);
+    return MaterialApp(
+          theme :ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+          debugShowCheckedModeBanner: false,
+          home: SplashScreen(),
+          );
+  }
+}
+
+class SplashScreen extends StatefulWidget{
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>{
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children:<Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.blueAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.shopping_cart,
+                          color: Colors.greenAccent,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "Kuwento",style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize:24.0,
+                                  fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
